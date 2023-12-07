d33f285cf7df66338edd972863ba11209ce56da7
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2023-12-07 15:34:28 +0100

    added the error to the asymmetry formula

--- src/muon/muon_flux_asymmetry.py
+++ src/muon/muon_flux_asymmetry.py
@@ -1,19 +1,24 @@
-import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
-from scipy.optimize import curve_fit
 
 # measured parameters
-counts_east = 826
-time_east = 4411 # in seconds
 counts_west = 49876
 time_west = 175421 # in seconds
+error_counts_west = np.sqrt(counts_west)
+
+counts_east = 1612
+time_east = 8815 # in seconds
+error_counts_east = np.sqrt(counts_east)
 
 # Calculate the rates
 west_rate = counts_west / time_west
-east_rate = counts_east / time_east
+error_west_rate = error_counts_west / time_west
 
+east_rate = counts_east / time_east
+error_east_rate = error_counts_east / time_west
+
+error_rate = np.sqrt((error_east_rate) ** 2 + (error_west_rate)** 2)
 # Asymmetry formula
 A = (west_rate - east_rate) / (west_rate + east_rate)
+error_A = A * np.sqrt(((error_rate)/(west_rate - east_rate))**2 + ((error_rate)/(west_rate + east_rate))**2)
 
-print(f"The asymmetry is {round(A* 100, 1)}%")
+print(f"The asymmetry is {round(A * 100, 1)} ± {round(error_A * 100, 1)}%")
