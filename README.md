89b3511ce3c3975b14833a863d3a13a2025b9af6
lesliewu0506 <135598464+lesliewu0506@users.noreply.github.com>
2023-12-16 11:45:56 +0100

    readme added

---
+++ README.md
@@ -0,0 +1,9 @@
+If there is an import error with the line 
+```python
+from Lifetime_analyse import tau, tau_error
+```
+or 
+```python
+from muon.Lifetime_analyse import tau, tau_error
+```
+interchange them to fix it.
--- src/muon/Delta time analyse.py
+++ src/muon/Delta time analyse.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from Lifetime_analyse import tau, tau_error
-from Lifetime_analyse import tau, tau_error
 from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
 
 # from muon.Lifetime_analyse import tau, tau_error
+from Lifetime_analyse import tau, tau_error
 
 # Load your data from the CSV file
 data_pos = pd.read_csv("Delta time groep A 30-11.csv")
